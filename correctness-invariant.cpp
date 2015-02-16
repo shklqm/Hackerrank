32ba1d073b33fdf2d55ef73d46ed21de52a4be53
Shkelqim Memolla <shkelqim.memolla@gmail.com>
2015-02-16 16:29:13 +0200

    Create correctness-invariant.cpp

---
+++ correctness-invariant.cpp
@@ -0,0 +1,49 @@
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <ctime>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <bitset>
+#include <cstdio>
+#include <vector>
+#include <cstdlib>
+#include <numeric>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+void insertionSort(int ar_size, int *  ar) 
+{
+	for(int i=1; i<ar_size; i++)
+	{
+		int elem = ar[i];
+		
+		int j;
+		for( j=i; j>0 && elem < ar[j-1]; j-- )
+				ar[j] = ar[j-1];
+	
+		ar[j] = elem;	
+	
+	}
+	
+	for(int j=0; j<ar_size; j++)
+			cout<<ar[j]<<" ";
+		cout<<endl;	
+
+}
+int main(void) {
+   
+    int _ar_size;
+    cin >> _ar_size;
+    int _ar[_ar_size], _ar_i;
+    for(_ar_i = 0; _ar_i < _ar_size; _ar_i++) 
+        cin >> _ar[_ar_i];
+
+   insertionSort(_ar_size, _ar);
+   
+   return 0;
+}
